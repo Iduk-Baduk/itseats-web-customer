24fc98ee8e1834cc3b59120f3dc9441d4cb257c5
Woochang Lee <changi112242@gmail.com>
2025-06-17 00:04:40 +0900

    fix: motion.div로 인한 스크롤 문제 해결 2
    height: 100dvh 제거

--- src/components/animation/SlideInFromRight.jsx
+++ src/components/animation/SlideInFromRight.jsx
@@ -15,10 +15,7 @@ export default function SlideInFromRight({ children }) {
       animate={{ x: 0 }}
       transition={{ duration: 0.25, ease: "easeInOut" }}
       style={{
-        height: "100dvh",
         backgroundColor: "#ffffff",
-        boxShadow:
-          "0 10px 15px -3px rgba(0,0,0,0.1), 0 4px 6px -4px rgba(0,0,0,0.1)",
         overflow: isAnimating ? "hidden" : "unset",
       }}
     >
