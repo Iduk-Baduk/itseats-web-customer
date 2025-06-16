354664faeb0a801fecdb429705b4016f3b7689fe
한상희 <tkdgml822@gmail.com>
2025-06-16 16:14:56 +0900

    feat: css적용 최근 검색어 구현중

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,61 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import SearchInput from "../components/common/SearchInput";
 import styles from "./Search.module.css";
 
 export default function Search() {
+  // 초기 데이터
+  const [recentKeywords, setRecentKeywords] = useState([
+    {keyword: "삽겹살", date: "06.01"},
+    {keyword: "햄버거", date: "05.31"},
+    
+  ]);
+  
+
   return (
-    <div className="search-page">
+    // 상단 검색 바
+    <div>
       <div className={styles.container}>
         <button className={styles.backBtn}>←</button>
         <SearchInput className={styles.searchInput} />
         <button className={styles.searchBtn}>🔍</button>
       </div>
 
-      <div className="section popular">
-        <div className="section-title">
-          <span>인기 검색어</span>
-          <span className="update-time">오후 4:10 업데이트</span>
-        </div>
-        <div className="popular-keywords">
-          {/* 좌우 2열 */}
-          <div className="column">
-            <div>1. 메가커피</div>
-            <div>2. 맥도날드</div>
-            <div>3. 롯데리아</div>
-            <div>4. 냉면</div>
-            <div>5. 교촌치킨</div>
-          </div>
-          <div className="column">
-            <div>6. 맘스터치</div>
-            <div>7. 김밥</div>
-            <div>8. 서브웨이</div>
-            <div>9. 마라탕</div>
-            <div>10. 햄버거</div>
-          </div>
+      {/* 인기 검색어 및 날짜 */}
+      <div>
+        <div className={styles.keywordHeader}>
+          <span className={styles.title}>인기 검색어</span>
+          <span className={styles.subTextRight}>오후 4:10 업데이트</span>
         </div>
       </div>
 
-      <div className="section recent">
-        <div className="section-title">
-          <span>최근 검색어</span>
-          <button className="clear-all">전체삭제</button>
-        </div>
-        <ul className="recent-list">
-          <li>
-            삼겹살 <span className="date">06.01</span> <button>✕</button>
-          </li>
-          <li>
-            햄버거 <span className="date">05.31</span> <button>✕</button>
-          </li>
-        </ul>
-      </div>
+      <hr/>
 
-      <div className="bottom-nav">
-        <button>홈</button>
-        <button>검색</button>
-        <button>즐겨찾기</button>
-        <button>주문내역</button>
-        <button>My 이츠</button>
+      {/* 최근 검색어 */}
+      <div>
+        <div className={styles.keywordHeader}>
+          <span className={styles.title}>최근 검색어</span>
+          <button className={styles.subTextRight}>전체삭제</button>
+        </div>
+        
+          <ul className={styles.recentList}>
+            <ul className={styles.recentList}>
+              {recentKeywords.map((item, index) => (
+                <li key={index} className={styles.recentItem}>
+                  <div className={styles.left}>
+                    <span className={styles.clockIcon}>🕑</span>
+                    <span>{item.keyword}</span>
+                  </div>
+                  <div className={styles.right}>
+                    <span className={styles.date}>{item.date}</span>
+                    <button className={styles.deleteBtn}>✕</button>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </ul>
+        
       </div>
     </div>
   );
