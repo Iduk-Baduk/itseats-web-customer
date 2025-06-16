eb463133cf414266a582f9beadb59860aa239e84
chk-jk <chk-jk@github.com>
2025-06-16 10:50:54 +0900

    회원가입 페이지 추가

---
+++ src/pages/regist.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Order() {
+  return (
+    <div>
+      <div style={{ width: "100%", height: "100vh", display: "flex", justifyContent: "center", alignItems: "center" }}>
+        <h1>회원가입</h1>
+        <SignUpForm />
+      </div>
+    </div>
+  );
+}
