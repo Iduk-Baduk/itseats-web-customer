0cee57125b9655a4ba55b0779b931a67a181327b
Woochang Lee <changi112242@gmail.com>
2025-06-16 23:55:03 +0900

    fix: motion.div로 인한 스크롤 문제 해결
    애니메이션 전후 height가 변동으로 인해 overflow: hidden을 넣으니, 스크롤이 안되는 문제 발생 -> 애니메이션 종료 후 overflow: unset으로 설정

--- src/components/animation/SlideInFromRight.jsx
+++ src/components/animation/SlideInFromRight.jsx
@@ -1,7 +1,14 @@
 import { motion } from "framer-motion";
-import styles from "../../layouts/Layout.module.css";
+import { useEffect, useState } from "react";
 
 export default function SlideInFromRight({ children }) {
+  const [isAnimating, setIsAnimating] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setIsAnimating(false), 250);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <motion.div
       initial={{ x: "100%" }}
@@ -12,7 +19,7 @@ export default function SlideInFromRight({ children }) {
         backgroundColor: "#ffffff",
         boxShadow:
           "0 10px 15px -3px rgba(0,0,0,0.1), 0 4px 6px -4px rgba(0,0,0,0.1)",
-        overflow: "hidden",
+        overflow: isAnimating ? "hidden" : "unset",
       }}
     >
       {children}
