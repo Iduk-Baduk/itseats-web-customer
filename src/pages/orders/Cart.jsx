e351edb62f0847ff08676da98873cf0bc17ffac0
Woochang Lee <changi112242@gmail.com>
2025-06-19 13:41:45 +0900

    chore: 불필요한 import 삭제

--- src/pages/orders/Cart.jsx
+++ src/pages/orders/Cart.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import Header from "../../components/common/Header";
 import TextInput from "../../components/common/basic/TextInput";
