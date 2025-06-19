0a2819c92fe41c2d48327868f1db5c90db00b476
한상희 <tkdgml822@gmail.com>
2025-06-19 13:03:43 +0900

    feat: 리뷰 상세 입력 창 구현

--- src/pages/Review.jsx
+++ src/pages/Review.jsx
@@ -1,11 +1,16 @@
 import ReviewCard from "../components/review/ReviewCard";
+import ReviewTextarea from "../components/review/ReviewTextarea";
+
 import styles from "./Review.module.css";
 
 export default function Review() {
   return (
     <div className={styles.reviewContainer}>
-      <ReviewCard object="음식" image="example.jpg" className={styles.reviewCard} />
-      <ReviewCard object="배달" image="example.jpg" className={styles.reviewCard} />
+      <ReviewCard object="음식" image="example.jpg" />
+      <ReviewCard object="배달" image="example.jpg" />
+      <p>배달파트너에 대해 평가해주세요 (선택)</p>
+      <hr className={styles.hr} />
+      <ReviewTextarea className={styles.reviewTextarea} />
     </div>
   );
 }
