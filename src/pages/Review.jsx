89e0175205fe27dc7e868080e560c8b1ca8e4d6e
한상희 <tkdgml822@gmail.com>
2025-06-19 14:34:22 +0900

    feat: 리뷰 항목 UI 컴포넌트 구현

--- src/pages/Review.jsx
+++ src/pages/Review.jsx
@@ -1,18 +1,27 @@
 import PhotoButton from "../components/review/PhotoButton";
 import ReviewCard from "../components/review/ReviewCard";
 import ReviewTextarea from "../components/review/ReviewTextarea";
+import ReviewItem from "../components/review/ReviewItem";
 
 import styles from "./Review.module.css";
 
 export default function Review() {
   return (
     <div className={styles.reviewContainer}>
-      <ReviewCard object="음식" image="example.jpg" />
-      <ReviewCard object="배달" image="example.jpg" />
-      <p>배달파트너에 대해 평가해주세요 (선택)</p>
+      <ReviewCard object="음식" image={"https://example.com/image.jpg"} />
+      <ReviewCard object="배달" image={"https://example.com/image.jpg"} />
+      <p className={styles.text}>배달파트너에 대해 평가해주세요 (선택)</p>
       <hr className={styles.hr} />
       <ReviewTextarea className={styles.reviewTextarea} />
       <PhotoButton />
+      <ReviewItem
+        imageUrl={"https://example.com/image.jpg"}
+        name="[오] 필라델피아 치즈 스테이크 (L)"
+        option="코카콜라 제로 1.25L"
+        onLike={() => {}}
+        onDislike={() => {}}
+        className={styles.reviewItem}
+      />
     </div>
   );
 }
