e708c618ccc25705b4fb88013e157ee00ef5d933
Woochang Lee <changi112242@gmail.com>
2025-06-19 13:11:05 +0900

    feat: 결제금액 표시 및 요청사항 입력 기능 추가

--- src/pages/orders/Cart.jsx
+++ src/pages/orders/Cart.jsx
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import Header from "../../components/common/Header";
+import TextInput from "../../components/common/basic/TextInput";
+import CheckBox from "../../components/common/basic/Checkbox";
 import DeliveryToggle from "../../components/orders/cart/DeliveryToggle";
 import DeliveryRadioButton from "../../components/orders/cart/DeliveryRadioButton";
 import QuantityControl from "../../components/orders/cart/QuantityControl";
+import RiderRequestBottomSheet from "../../components/orders/cart/RiderRequestBottomSheet";
 import styles from "./Cart.module.css";
 
 export default function Cart() {
@@ -12,9 +15,15 @@ export default function Cart() {
   const [isDelivery, setIsDelivery] = useState("delivery"); // "delivery" or "takeout"
   const [deliveryType, setDeliveryType] = useState("default"); // "default" or "onlyone"
   const [orderMenus, setOrderMenus] = useState(dummyOrder.orderMenus);
+  const [storeRequest, setStoreRequest] = useState("");
+  const [chopsticks, setChopsticks] = useState(false);
+  const [riderRequest, setRiderRequest] =
+    useState("직접 받을게요 (부재 시 문 앞)");
+  const [riderRequestSelf, setRiderRequestSelf] = useState("");
+  const [isRiderRequestSheetOpen, setRiderRequestSheetOpen] = useState(false);
 
   // 수량 변경에 따른 가격 업데이트
-  const updateOptionTotalPrice = () => {
+  const updateMenuTotalPrice = () => {
     setOrderMenus((prevMenus) =>
       prevMenus.map((menu) => {
         const basePrice = menu.menuPrice;
@@ -29,7 +38,7 @@ export default function Cart() {
 
         return {
           ...menu,
-          optionTotalPrice: (basePrice + optionsPrice) * menu.quantity,
+          menuTotalPrice: (basePrice + optionsPrice) * menu.quantity,
         };
       })
     );
@@ -56,7 +65,7 @@ export default function Cart() {
           })
           .filter(Boolean) // null 제거 (삭제된 항목)
     );
-    updateOptionTotalPrice();
+    updateMenuTotalPrice();
   }
 
   return (
@@ -150,7 +159,7 @@ export default function Cart() {
                   </React.Fragment>
                 ))}
                 <p className={styles.menuPrice}>
-                  {menu.optionTotalPrice.toLocaleString()}원
+                  {menu.menuTotalPrice.toLocaleString()}원
                 </p>
               </div>
             </div>
@@ -200,9 +209,99 @@ export default function Cart() {
           </svg>
         </h2>
       </section>
-      <section>
+      <section style={{ borderBottomWidth: "24px" }}>
         <h2>결제금액</h2>
+        <div className={styles.paymentDetails}>
+          <div className={styles.paymentRow}>
+            <span>주문금액</span>
+            <span>{dummyResponse.orderPrice.toLocaleString()}원</span>
+          </div>
+          {isDelivery === "delivery" && (
+            <div className={styles.paymentRow}>
+              <span>배달비</span>
+              <span>{dummyResponse.deliveryFee.toLocaleString()}원</span>
+            </div>
+          )}
+          <div className={styles.paymentRow}>
+            <span>할인금액</span>
+            <span style={{ color: "var(--minus-red)" }}>
+              -{dummyResponse.discountValue.toLocaleString()}원
+            </span>
+          </div>
+          <div className={styles.totalCost}>
+            <span>총 결제금액</span>
+            <span>{dummyResponse.totalCost.toLocaleString()}원</span>
+          </div>
+        </div>
       </section>
+      <section style={{ borderBottomWidth: "24px" }}>
+        <h2>결제수단</h2>
+      </section>
+      <section>
+        <h2>요청사항</h2>
+        <div>
+          <p className={styles.requestLabel}>가게 사장님께</p>
+          <TextInput
+            placeholder="가게 요청사항을 입력해주세요"
+            className={styles.requestInput}
+            value={storeRequest}
+            onChange={(e) => setStoreRequest(e.target.value)}
+            maxLength={50}
+          />
+          <CheckBox
+            label="일회용 수저, 포크 받기"
+            checked={chopsticks}
+            onChange={(e) => setChopsticks(e.target.checked)}
+          />
+        </div>
+        {isDelivery === "delivery" && (
+          <div>
+            <p className={styles.requestLabel}>배달파트너님께</p>
+            <div className={styles.riderRequestSelect}>
+              <input
+                className={styles.select}
+                type="text"
+                onClick={() => setRiderRequestSheetOpen(true)}
+                value={riderRequest}
+                readOnly
+              />
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  fill="currentColor"
+                  d="M15.88 9.29L12 13.17L8.12 9.29a.996.996 0 1 0-1.41 1.41l4.59 4.59c.39.39 1.02.39 1.41 0l4.59-4.59a.996.996 0 0 0 0-1.41c-.39-.38-1.03-.39-1.42 0"
+                />
+              </svg>
+            </div>
+            {riderRequest === "직접 입력하기" && (
+              <>
+                <TextInput
+                  placeholder="배달 요청사항을 입력해주세요"
+                  className={styles.requestInput}
+                  value={riderRequestSelf}
+                  onChange={(e) => setRiderRequest(e.target.value)}
+                  maxLength={50}
+                />
+                <p className={styles.deliveryInfo}>
+                  모든 주문은 배달완료 사진을 보내드립니다.
+                </p>
+              </>
+            )}
+          </div>
+        )}
+      </section>
+      <RiderRequestBottomSheet
+        request={riderRequest}
+        isOpen={isRiderRequestSheetOpen}
+        onClose={() => setRiderRequestSheetOpen(false)}
+        onSelect={(request) => {
+          setRiderRequest(request);
+        }}
+      />
     </div>
   );
 }
@@ -243,7 +342,7 @@ const dummyOrder = {
           ],
         },
       ],
-      optionTotalPrice: 7000,
+      menuTotalPrice: 7000,
       quantity: 1,
     },
     {
@@ -269,7 +368,7 @@ const dummyOrder = {
           options: [],
         },
       ],
-      optionTotalPrice: 8000,
+      menuTotalPrice: 8000,
       quantity: 2,
     },
   ],
