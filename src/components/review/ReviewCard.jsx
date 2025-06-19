130ce7fe8ef1b3546a63731291635dfe93042625
한상희 <tkdgml822@gmail.com>
2025-06-19 16:46:16 +0900

    feat: 라이더 아이콘 추가

--- src/components/review/ReviewCard.jsx
+++ src/components/review/ReviewCard.jsx
@@ -1,6 +1,17 @@
 import { useState } from "react";
 import styles from "./ReviewCard.module.css";
 
+const RiderIcon = ({ className }) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" className={className} viewBox="0 0 24 24">
+      <path
+        fill="#199dff"
+        d="M15.5 5.5c1.1 0 2-.9 2-2s-.9-2-2-2s-2 .9-2 2s.9 2 2 2M5 12c-2.8 0-5 2.2-5 5s2.2 5 5 5s5-2.2 5-5s-2.2-5-5-5m0 8.5c-1.9 0-3.5-1.6-3.5-3.5s1.6-3.5 3.5-3.5s3.5 1.6 3.5 3.5s-1.6 3.5-3.5 3.5m5.8-10l2.4-2.4l.8.8c1.3 1.3 3 2.1 5.1 2.1V9c-1.5 0-2.7-.6-3.6-1.5l-1.9-1.9c-.5-.4-1-.6-1.6-.6s-1.1.2-1.4.6L6.31 9.9L11 14v5h2v-6.2zM19 12c-2.8 0-5 2.2-5 5s2.2 5 5 5s5-2.2 5-5s-2.2-5-5-5m0 8.5c-1.9 0-3.5-1.6-3.5-3.5s1.6-3.5 3.5-3.5s3.5 1.6 3.5 3.5s-1.6 3.5-3.5 3.5"
+      />
+    </svg>
+  );
+};
+
 const YellowStarIcon = ({ className }) => {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" className={className} viewBox="0 0 24 24">
@@ -59,7 +70,13 @@ export default function ReviewCard({ object, image, className, onSelect }) {
           </div>
         </div>
 
-        <img src={image.url} alt="리뷰 대상" className={styles.image} />
+        {object === "음식" ? (
+          <img src={image.url} alt="리뷰 대상" className={styles.image} />
+        ) : (
+          <div className={styles.image}>
+            <RiderIcon className={styles.riderIcon} />
+          </div>
+        )}
       </div>
     </div>
   );
