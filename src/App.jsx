c88bce3ccba8b99dc5a597bb381310e503647f5f
chk-jk <chk-jk@github.com>
2025-06-20 15:44:57 +0900

    fix: provider 중첩 제거

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
 // src/App.jsx
 import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux"; // ✅ 추가
-import store from "./store"; // ✅ store import
 import Root from "./Root";
 
 export default function App() {
   return (
-    <Provider store={store}> {/* ✅ Redux Provider로 감싸기 */}
-      <BrowserRouter>
-        <Root />
-      </BrowserRouter>
-    </Provider>
+    <BrowserRouter>
+      <Root />
+    </BrowserRouter>
   );
 }
