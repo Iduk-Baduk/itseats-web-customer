d57dde5a93652bedae5d73199269ca3b708c7cd6
chk-jk <chk-jk@github.com>
2025-06-23 13:52:40 +0900

    feat: 장바구니 리덕스 연동

--- src/Store.js
+++ src/Store.js
@@ -1,12 +1,15 @@
 // src/Store.js
 import { configureStore } from "@reduxjs/toolkit";
 import rootReducer from "./modules";
+import { loadCart } from "./store/localStorage";
+
 
 // ✅ localStorage에서 count 불러오기
 const savedCount = localStorage.getItem("count");
 const preloadedState = {
   counter: {
     count: savedCount ? parseInt(savedCount, 10) : 0,
+    cart: { orderMenus: loadCart() },
   },
 };
 
