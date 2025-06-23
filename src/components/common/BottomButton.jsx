98f9bb5e90a296b3d2e6e4b00fd04affcf7fc5b3
chk-jk <chk-jk@github.com>
2025-06-23 17:38:44 +0900

    chore: 바텀 버튼 CSS 수정

--- src/components/common/BottomButton.jsx
+++ src/components/common/BottomButton.jsx
@@ -17,8 +17,7 @@ export default function BottomButton({
 
   // ✅ 현재 경로가 /cart이고, 장바구니에 아이템이 있을 때만 "결제하기" 텍스트로 대체
   const isCartPage = location.pathname === "/cart";
-  const showPaymentText =
-    isCartPage && cartInfo?.itemCount > 0;
+  const showPaymentText = isCartPage && cartInfo?.itemCount > 0;
 
   return (
     <button
@@ -31,7 +30,11 @@ export default function BottomButton({
       {children}
 
       {!children && cartInfo && (
-        <div className={styles.flexContainer}>
+        <div
+          className={`${styles.flexContainer} ${
+            showPaymentText ? styles.centered : ""
+          }`}
+        >
           {showPaymentText ? (
             <span className={styles.paymentText}>
               {cartInfo.totalPrice.toLocaleString()}원 결제하기
