ff1fadb535aa5481355e03ceeafea5b3cb18d358
chk-jk <chk-jk@github.com>
2025-06-23 09:52:16 +0900

    chore: 로컬네트워크 접속 허용

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: true, //로컬 네트워크 접속 허용
     proxy: {
       "/api": {
         target: "http://localhost:3001", // JSON Server 포트
