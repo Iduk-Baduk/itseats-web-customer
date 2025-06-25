2fdc3aa99146be34c6691c5b5025e7c990bd1302
chk-jk <chk-jk@github.com>
2025-06-25 10:51:49 +0900

    CartBottomBar 중복 제거 및 Cart.jsx에서 import/사용 삭제

---
+++ src/components/orders/cart/CartAddressSection.jsx
@@ -0,0 +1,4 @@
+import React from 'react';
+export default function CartAddressSection() {
+  return <section>배달지/주소 정보</section>;
+} 
---
+++ src/components/orders/cart/CartPaymentMethodSection.jsx
@@ -0,0 +1,4 @@
+import React from 'react';
+export default function CartPaymentMethodSection() {
+  return <section>결제수단 선택</section>;
+} 
